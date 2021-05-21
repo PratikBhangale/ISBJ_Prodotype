e73e25e87a1093689f821e3d8ea4de203d5a6ccd
Pratik Bhangale <47623210+PratikBhangale@users.noreply.github.com>
2021-05-21 22:32:15 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,7 @@
+const apperels = document.querySelector('#navbarDropdownMenuLink');
+
+const toApperels = function () {
+        window.location = 'apperels.html';
+}   
+
+apperels.addEventListener('click', toApperels);
