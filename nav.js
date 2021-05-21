fad7317895b610defbb3f146f2c0ae39612547ff
Pratik Bhangale <47623210+PratikBhangale@users.noreply.github.com>
2021-05-21 14:45:27 +0530

    Delete nav.js

--- nav.js
+++
@@ -1,7 +0,0 @@
-const apperels = document.querySelector('#navbarDropdownMenuLink');
-
-const toApperels = function () {
-        window.location = 'apperels.html';
-}   
-
-apperels.addEventListener('click', toApperels);
